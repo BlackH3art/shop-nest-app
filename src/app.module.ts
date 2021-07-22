5e2b4fd25e678920b32bc1c243488790d31b6e63
BlackH3art <Jaroslaw.Musielak@outlook.com>
2021-07-22 16:30:44 +0200

    Users module + connection with SQL db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BasketModule } from './basket/basket.module';
 import { ShopModule } from './shop/shop.module';
+import { UsersModule } from './users/users.module';
 
 // usunięcie BasketController i BasketService - wydzielenie osobego modułu.
 // usunięcie ShopController i ShopService - wydzielenie osobnego modułu.
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(),
     BasketModule,
     ShopModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
